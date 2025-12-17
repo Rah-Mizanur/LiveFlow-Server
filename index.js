fccdb4d2a86fcc19bd7a87ff5052158bb82b9157
unknown <mijan90786@gmail.com>
2025-12-18 01:41:25 +0600

    add role based API

--- index.js
+++ index.js
@@ -140,6 +140,11 @@ async function run() {
       res.send(result);
     });
 
+     app.get("/user/role", verifyJWT, async (req, res) => {
+      const result = await usersCollection.findOne({ email: req.tokenEmail });
+      res.send({ role: result?.role });
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
